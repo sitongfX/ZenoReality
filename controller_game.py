970d4499095e294173f8f5898b7d3efe2fdf1402
sitongfx <sf3049@columbia.edu>
2022-05-12 19:53:49 -0400

    reformatted

--- controller_game.py
+++ controller_game.py
@@ -10,171 +10,152 @@ from connecting_serial_controller import connect_ser
 # two parts
 # 1. get message from json manually
 
-  # functions: 
-  # potentiometer = camera height
-  # joystick = direction of the player
-  # button = rewind the time
+# functions:
+# potentiometer = camera height
+# joystick = direction of the player
+# button = rewind the time
 
 ser = connect_ser()
 
 # 2. the game
 
 
+while True:
 
-while(True):
+    app = Ursina()
 
-  app  =  Ursina()
+    player = FirstPersonController(collider="box", speed=5, scale=2, max_jumps=2)
+    B = Button(scale=0.1, icon="sword", position=(0.65, 0.4))
+    B.on_click = B.fade_out(duration=0.5)
 
-  player  =  FirstPersonController(
-    collider = 'box',
-    speed = 5,
-    scale = 2,
-    max_jumps=2
-  )
-  B = Button(scale=0.1, icon="sword", position = (0.65, 0.4))
-  B.on_click = B.fade_out(duration=0.5)
+    ground = Entity(model="plane", texture="grass", collider="mesh", scale=(30, 0, 3))
 
-  ground  =  Entity(
-    model = 'plane',
-    texture = 'grass',
-    collider = 'mesh',
-    scale = (30,0,3)
-  )
+    bridge1 = Entity(model="cube", texture="brick", scale=(0.4, 0.1, 53), z=28, x=-0.7)
 
-  bridge1  =  Entity(
-    model = 'cube',
-    texture = 'brick',
-    scale = (0.4,0.1,53),
-    z = 28,x = -0.7
-  )
+    bridge2 = duplicate(bridge1, x=-3.7)
+    bridge3 = duplicate(bridge1, x=0.6)
+    bridge4 = duplicate(bridge1, x=3.6)
 
-  bridge2 = duplicate(bridge1, x = -3.7)
-  bridge3 = duplicate(bridge1, x = 0.6)
-  bridge4 = duplicate(bridge1, x = 3.6)
+    blocks = []
+    for i in range(12):
+        block = Entity(
+            model="cube",
+            collider="box",
+            color=color.white33,
+            position=(2, 0.1, 3 + i * 4),
+            scale=(3, 0.1, 2.5),
+        )
+        block2 = duplicate(block, x=-2.2)
 
-  blocks = []
-  for i in range(12):
-    block = Entity(
-      model = 'cube', 
-      collider = 'box',
-      color = color.white33,
-      position = (2,0.1,3+i*4),
-      scale = (3,0.1,2.5)
-    )
-    block2 = duplicate(block, x = -2.2)
+        blocks.append((block, block2, randint(0, 3) > 0, randint(0, 3) > 0))
 
-    blocks.append(
-      (block, block2, randint(0,3)>0,randint(0,3)>0)
+    goal = Entity(
+        color=color.brown, model="cube", collider="box", scale=(10, 1, 10), z=55
     )
 
+    goal_animate = Sequence(
+        1, Func(goal.blink, duration=5), Func(goal.shake, duration=15), Loop=True
+    )
+    goal_animate.start()
 
-  goal = Entity(
-    color = color.brown,
-    model='cube',
-    collider = 'box',
-    scale=(10,1,10), 
-    z=55
-  )
+    # audio
+    a = Audio("yamato", pitch=1, loop=True, autoplay=True)
+    a.volume = 20
 
-  goal_animate = Sequence(1, Func(goal.blink, duration = 5), Func(goal.shake, duration=15), Loop = True)
-  goal_animate.start()
+    def input(key):
+        if key == "m":
+            a.volume = 0
+        if key == "c":
+            goal.color = color.rgb(randint(0, 255), randint(0, 255), randint(0, 255))
 
-  # audio
-  a = Audio('yamato', pitch=1, loop=True, autoplay=True)
-  a.volume = 20 
+    def update():
 
-  def input(key):
-      if key == 'm':
-          a.volume = 0
-      if key == 'c':
-          goal.color = color.rgb(randint(0,255), randint(0,255),randint(0,255))
+        try:
+            con = []  # list of values from controllers: P, X, Y, B
+            data = str(ser.readline().strip())[3:-2]
+            items = data.split(",")
+            for i in items:
+                temp = i.split(":")
+                con.append(int(temp[1]))
+            # print(con)
 
+            if len(con) == 4:  # make sure data is not corrupted
 
-  def update():
+                camera.position = (0, time.dt * con[0], 0)
 
-    try:
-      con  =  []  # list of values from controllers: P, X, Y, B
-      data  =  str(ser.readline().strip())[3:-2]
-      items  =  data.split(',')
-      for i in items:
-        temp  =  i.split(":")
-        con.append(int(temp[1]))  
-      # print(con)
+                if con[1] > 75:  # joy stick X
+                    move_right = True
+                elif con[1] < 25:
+                    move_left = True
+                else:
+                    move_right = False
+                    move_left = False
 
-      if len(con) == 4:  # make sure data is not corrupted
+                if con[2] > 75:  # joy stick Y
+                    move_forward = True
+                elif con[2] < 25:
+                    move_back = True
+                else:
+                    move_forward = False
+                    move_back = False
 
-        camera.position = (0, time.dt * con[0] , 0) 
+                if move_right:
+                    player.x += time.dt * player.speed  # go right
 
-        if con[1] > 75:  # joy stick X
-          move_right = True
-        elif con[1] < 25:
-          move_left = True
-        else:
-          move_right = False
-          move_left = False
+                if move_left:
+                    player.x -= time.dt * player.speed  # go left
 
-        if con[2] > 75:   # joy stick Y
-          move_forward = True
-        elif con[2] < 25:
-          move_back = True
-        else:
-          move_forward = False
-          move_back = False
+                if move_forward:
+                    player.z += time.dt * player.speed  # go forward
 
+                if move_back:
+                    player.z -= time.dt * player.speed  # go backward
 
-        if (move_right):
-            player.x += time.dt * player.speed  # go right
-        
-        if (move_left):
-            player.x -= time.dt * player.speed   # go left
+                if held_keys["space"]:
+                    player.z += time.dt * player.speed * 2.5  # dash forward
 
-        if (move_forward):
-            player.z += time.dt * player.speed     # go forward
-        
-        if (move_back):
-            player.z -= time.dt * player.speed   # go backward
+                if con[3]:
+                    rewind_dis = randint(0, 100) * 0.1
+                    if rewind_dis > firstMan.z:
+                        while firstMan.z > 0:
+                            firstMan.z -= 0.05
+                    else:
+                        firstMan.z = firstMan.z - rewind_dis
+                    if rewind_dis > player.z:
+                        while player.z > 0:
+                            player.z -= 0.01
+                    else:
+                        player.z = player.z - rewind_dis
 
-        if (held_keys['space']):
-            player.z += time.dt * player.speed * 2.5  # dash forward
+                firstMan.z = firstMan.z + time.dt * 2.05
 
-        if con[3]:
-            rewind_dis = (randint(0,100)*0.1)
-            if rewind_dis > firstMan.z:
-              while firstMan.z > 0:
-                firstMan.z -= 0.05
-            else:
-              firstMan.z = firstMan.z - rewind_dis
-            if rewind_dis > player.z:
-                while player.z > 0:
-                  player.z -= 0.01
-            else:
-              player.z = player.z - rewind_dis
+                if player.y < -5:
+                    Text(
+                        text="You died for a great cause!",
+                        origin=(0, 0),
+                        scale=2,
+                        color=color.orange,
+                    )
 
+        except Exception:
+            pass
 
-        firstMan.z = firstMan.z + time.dt*2.05
+    class Man(FrameAnimation3d):
+        def __init__(self):
+            super().__init__(
+                "walking_",
+                fps=100,
+                frame_times=138,
+                autoplay=True,
+                loop=True,
+                scale=4,
+                position=(-2, 0.5, -5),
+                texture="Uriel_diffuse",
+            )
 
-        if player.y < -5:
-          Text(text="You died for a great cause!", origin=(0,0), scale=2, color = color.orange)  
-    
-    except Exception:
-      pass
-
-  class Man(FrameAnimation3d):
-    def __init__(self):
-        super().__init__(
-          "walking_",
-          fps = 100,
-          frame_times = 138,
-          autoplay = True,
-          loop = True,
-          scale = 4,
-          position = (-2,0.5,-5),
-          texture = "Uriel_diffuse"
-        )  
-
-  firstMan = Man()
-  window.title = "Zeno's reality"
-  Sky(texture = "sky_sunset", rotation = (0,90,0))
-  scene.fog_density = .1          # sets exponential density
-  scene.fog_density = (50, 200) 
-  app.run()
+    firstMan = Man()
+    window.title = "Zeno's reality"
+    Sky(texture="sky_sunset", rotation=(0, 90, 0))
+    scene.fog_density = 0.1  # sets exponential density
+    scene.fog_density = (50, 200)
+    app.run()
--- keyboard_game.py
+++ keyboard_game.py
@@ -7,137 +7,129 @@ from ursina.prefabs.first_person_controller import FirstPersonController
 
 
 def audio_adjust(key_input):
-    if key_input == 'm':
+    if key_input == "m":
         a.volume = 0
         return True
 
+
 def color_adjust(key_input):
-    if key_input == 'c':
-        goal.color = color.rgb(randint(0,255), randint(0,255),randint(0,255))
+    if key_input == "c":
+        goal.color = color.rgb(randint(0, 255), randint(0, 255), randint(0, 255))
         return True
+
 
 def input(key):
     audio_adjust(key)
     color_adjust(key)
 
+
 def update():
 
-  if held_keys['q']:                             # If q is pressed
-    camera.position += (0, 0.5*time.dt, 0)           # move up vertically
-  if held_keys['a']:                             # If a is pressed
-    camera.position -= (0, time.dt, 0)           # move down vertically
+    if held_keys["q"]:  # If q is pressed
+        camera.position += (0, 0.5 * time.dt, 0)  # move up vertically
+    if held_keys["a"]:  # If a is pressed
+        camera.position -= (0, time.dt, 0)  # move down vertically
 
-  player.x += held_keys['right arrow'] * time.dt * player.speed  # go right
-  
-  player.x -= held_keys['left arrow'] * time.dt * player.speed   # go left
+    player.x += held_keys["right arrow"] * time.dt * player.speed  # go right
 
-  player.z += held_keys['up arrow'] * time.dt * player.speed     # go forward
-  
-  player.z -= held_keys['down arrow'] * time.dt * player.speed   # go backward
+    player.x -= held_keys["left arrow"] * time.dt * player.speed  # go left
 
-  player.z += held_keys['space'] * time.dt * player.speed * 2.5  # dash forward
+    player.z += held_keys["up arrow"] * time.dt * player.speed  # go forward
 
-  firstMan.z = firstMan.z + time.dt*2.05
+    player.z -= held_keys["down arrow"] * time.dt * player.speed  # go backward
 
-  # if firstMan.z > player.z:
-  #    Text(text="You are a loser!", origin=(0,0), scale=2)
-  if player.y < -5:
-    Text(text="You died for a great cause!", origin=(0,0), scale=2, color = color.orange)  
+    player.z += held_keys["space"] * time.dt * player.speed * 2.5  # dash forward
 
-  if held_keys['r']:
-      rewind_dis = (randint(0,100)*0.1)
-      if rewind_dis > firstMan.z:
-        while firstMan.z > 0:
-          firstMan.z -= 0.05
-      else:
-        firstMan.z = firstMan.z - rewind_dis
-      # if rewind_dis > player.z:
-      #     while player.z > 0:
-      #       player.z -= 0.01
-      # else:
-      #   player.z = player.z - rewind_dis
+    firstMan.z = firstMan.z + time.dt * 2.05
 
-app  =  Ursina()
+    # if firstMan.z > player.z:
+    #    Text(text="You are a loser!", origin=(0,0), scale=2)
+    if player.y < -5:
+        Text(
+            text="You died for a great cause!",
+            origin=(0, 0),
+            scale=2,
+            color=color.orange,
+        )
 
-player  =  FirstPersonController(
-  collider = 'box',
-  speed = 5,
-  scale = 2,
-  max_jumps=2
-)
-B = Button(scale=0.1, icon="sword", position = (0.65, 0.4))
+    if held_keys["r"]:
+        rewind_dis = randint(0, 100) * 0.1
+        if rewind_dis > firstMan.z:
+            while firstMan.z > 0:
+                firstMan.z -= 0.05
+        else:
+            firstMan.z = firstMan.z - rewind_dis
+        # if rewind_dis > player.z:
+        #     while player.z > 0:
+        #       player.z -= 0.01
+        # else:
+        #   player.z = player.z - rewind_dis
+
+
+app = Ursina()
+
+player = FirstPersonController(collider="box", speed=5, scale=2, max_jumps=2)
+B = Button(scale=0.1, icon="sword", position=(0.65, 0.4))
 B.on_click = B.fade_out(duration=0.5)
 
-ground  =  Entity(
-  model = 'plane',
-  texture = 'grass',
-  collider = 'mesh',
-  scale = (30,0,3)
+ground = Entity(model="plane", texture="grass", collider="mesh", scale=(30, 0, 3))
+
+bridge1 = Entity(
+    model="cube",
+    # color = color.violet,
+    texture="brick",
+    scale=(0.4, 0.1, 53),
+    z=28,
+    x=-0.7,
 )
 
-bridge1  =  Entity(
-  model = 'cube',
-  # color = color.violet,
-  texture = 'brick',
-  scale = (0.4,0.1,53),
-  z = 28,x = -0.7
-)
-
-bridge2 = duplicate(bridge1, x = -3.7)
-bridge3 = duplicate(bridge1, x = 0.6)
-bridge4 = duplicate(bridge1, x = 3.6)
+bridge2 = duplicate(bridge1, x=-3.7)
+bridge3 = duplicate(bridge1, x=0.6)
+bridge4 = duplicate(bridge1, x=3.6)
 
 blocks = []
 for i in range(12):
-  block = Entity(
-    model = 'cube', 
-    collider = 'box',
-    color = color.white33,
-    position = (2,0.1,3+i*4),
-    scale = (3,0.1,2.5)
-  )
-  block2 = duplicate(block, x = -2.2)
+    block = Entity(
+        model="cube",
+        collider="box",
+        color=color.white33,
+        position=(2, 0.1, 3 + i * 4),
+        scale=(3, 0.1, 2.5),
+    )
+    block2 = duplicate(block, x=-2.2)
 
-  blocks.append(
-    (block, block2, randint(0,3)>0,randint(0,3)>0)
-  )
+    blocks.append((block, block2, randint(0, 3) > 0, randint(0, 3) > 0))
 
-goal = Entity(
-  color = color.brown,
-  collider = 'box',
-  model='cube',
-  scale=(10,1,10), 
-  z=55
+goal = Entity(color=color.brown, collider="box", model="cube", scale=(10, 1, 10), z=55)
+
+
+goal_animate = Sequence(
+    1, Func(goal.blink, duration=5), Func(goal.shake, duration=15), Loop=True
 )
-
-
-goal_animate = Sequence(1, Func(goal.blink, duration = 5), Func(goal.shake, duration=15), Loop = True)
 goal_animate.start()
 
 
-a = Audio('yamato', pitch=1, loop=True, autoplay=True)
+a = Audio("yamato", pitch=1, loop=True, autoplay=True)
 a.volume = 20
 
+
 class Man(FrameAnimation3d):
-  def __init__(self):
-      super().__init__(
-        "walking_",
-        fps = 100,
-        frame_times = 138,
-        autoplay = True,
-        loop = True,
-        scale = 4,
-        position = (-2,0.5,-5),
-        texture = "Uriel_diffuse"
-      )  
+    def __init__(self):
+        super().__init__(
+            "walking_",
+            fps=100,
+            frame_times=138,
+            autoplay=True,
+            loop=True,
+            scale=4,
+            position=(-2, 0.5, -5),
+            texture="Uriel_diffuse",
+        )
+
 
 firstMan = Man()
 window.title = "Zeno's reality"
-Sky(texture = "sky_sunset", rotation = (0,90,0))
-scene.fog_density = .1          # sets exponential density
-scene.fog_density = (50, 200) 
+Sky(texture="sky_sunset", rotation=(0, 90, 0))
+scene.fog_density = 0.1  # sets exponential density
+scene.fog_density = (50, 200)
 app.run()
-
-
-
-
