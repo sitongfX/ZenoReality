f44905e8d582d0cef7a35d8b6f65473b17b6a18d
sitongfx <sf3049@columbia.edu>
2022-05-12 20:01:58 -0400

    reformat

--- connecting_serial_controller.py
+++ connecting_serial_controller.py
@@ -3,10 +3,13 @@
 # change the port as necessary by your OS
 
 ser = None
+
+
 def connect_ser():
-  ser = True
-  return ser
-  # put in your own serial port
-  # ser  =  serial.Serial('/dev/cu.wchusbserial531C0151591', 250000)
- 
+    ser = True
+    return ser
+    # put in your own serial port
+    # ser  =  serial.Serial('/dev/cu.wchusbserial531C0151591', 250000)
+
+
 connect_ser()
--- keyboard_input.py
+++ keyboard_input.py
@@ -1,13 +1,17 @@
 volume = 40
 
+
 def audio_adjust(key_input):
-    if key_input == 'm':
+    if key_input == "m":
         volume = 0
         return volume
 
+
 color = 1
+
+
 def color_adjust(key_input):
-    if key_input == 'c':
+    if key_input == "c":
         color = 3
         # set color as color.rgb(randint(0,255), randint(0,255),randint(0,255))
         return color
