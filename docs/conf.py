fcb6dc5edcee1e9358a959bf903fec8b22bf98ed
sitongfx <sf3049@columbia.edu>
2022-05-12 22:02:58 -0400

    format

--- docs/conf.py
+++ docs/conf.py
@@ -58,12 +58,10 @@ from recommonmark.transform import AutoStructify
 
 def setup(app):
     app.add_config_value(
-
         "recommonmark_config", 
         {
             "auto_toc_tree_section": "Contents",
         },
-        
         True
     )
     app.add_transform(AutoStructify)
